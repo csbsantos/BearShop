afe88f4969561d8c5d1a1b96d36616792404c682
csbsantos <claudiasbsantos13@gmail.com>
2025-07-18 15:52:57 +0100

    Update Bear.java

--- src/Bear.java
+++ src/Bear.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Bear {
-}
+    private String typeOfBear;
+    public int battery;
+
+    public Bear(String typeOfBear, int battery) {
+        this.typeOfBear = typeOfBear;
+        this.battery = 100;
+    }
+    public void talk (){
+    }
+
+
+
+    }
