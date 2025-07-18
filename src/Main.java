5fb34ef49febc386f355c4cd7ee481b8509a75a6
csbsantos <claudiasbsantos13@gmail.com>
2025-07-18 15:58:46 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        BearShop bearShop1 = new BearShop(0);
+        BearShop bearShop2 = new BearShop(0);
+        bearShop1.createBears();
+        bearShop2.createBears();
+        bearShop1.compareShops(bearShop2);
     }
-}
+//        Bear bear1 = new SimpleBear("Simple Bear", 0);
+//
+//        Bear bear2 = new CrankyBear("Ted", 40);
+//        Bear bear3 = new DrunkBear("John", 10);
+//        for (int i = 0; i < 100; i++) {
+//            bear1.talk()
+//            ;
+//
+//        }
+    }
