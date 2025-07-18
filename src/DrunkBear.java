3ca390742aea312e40f3fae45b4626f923289663
csbsantos <claudiasbsantos13@gmail.com>
2025-07-18 15:58:22 +0100

    Update DrunkBear.java

--- src/DrunkBear.java
+++ src/DrunkBear.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
-public class DrunkBear {
+public class DrunkBear extends Bear{
+    private int sleep;
+
+
+    public DrunkBear(String typeOfBear, int battery) {
+        super(typeOfBear, battery);
+    }
+
+    @Override
+    public void talk (){
+        Random random= new Random();
+        int sleep= random.nextInt(100);
+        System.out.println("I love you");
+        battery--;
+        if (sleep==20){
+            System.out.println("Snooring.");
+
+        }
+    }
 }
