05bdb2f1b4e8bc96bf9b1c85b26db2be516cbfe9
csbsantos <claudiasbsantos13@gmail.com>
2025-07-18 15:53:50 +0100

    Update CrankyBear.java

--- src/CrankyBear.java
+++ src/CrankyBear.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+public class CrankyBear extends Bear{
+    private int counterSadSong=0;
 
-public class CrankyBear {
+    public CrankyBear(String typeOfBear, int battery) {
+        super(typeOfBear, battery);
+    }
+    @Override
+    public void talk (){
+        System.out.println("I love you");
+        battery--;
+        if (battery <50 && counterSadSong==0) {
+            System.out.println(" Singing a sad song");
+            counterSadSong++;
+        }else{
+            System.out.println("I can't talk anymore");
+        }
+    }
 }
